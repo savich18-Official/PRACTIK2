8699eb8ab82f1312110b3d1f76b9fdb397a7e1d7
@elena_savchenko <elenasav156@gmail.com>
2025-04-28 12:07:42 +0700

    Add fail main

--- DatabaseAndModels/mysite/shopapp/models.py
+++ DatabaseAndModels/mysite/shopapp/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone  # Для работы с временем
+from django.utils import timezone
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    is_archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS_CHOICES = [
@@ -9,10 +19,10 @@ class Order(models.Model):
         ('delivered', 'Доставлен'),
     ]
 
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)  # Пользователь, сделавший заказ
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES)  # Статус заказа
-    products = models.ManyToManyField(Product)  # Продукты в заказе
-    created_at = models.DateTimeField(default=timezone.now)  # Время создания заказа
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    products = models.ManyToManyField(Product)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Заказ #{self.id} от {self.customer.username}"
